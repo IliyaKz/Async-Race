c7cb8342828583f11087f054de214fe904b9765c
IliyaKz <iliya-kuznetsov@mail.ru>
2022-08-04 22:39:59 +0300

    feat: add storage

---
+++ async-race/src/components/storage.ts
@@ -0,0 +1,14 @@
+import { IStorage } from './interfacesAndTypes';
+
+export const storage = {
+  carsPage: 1,
+  cars: null,
+  carsCount: null,
+  winnersPage: 1, 
+  winners: null,
+  winnersCount: null,
+  animation: {},
+  view: 'garage',
+  sortBy: null,
+  sortOrder: null,
+} as IStorage;
