62e83d3065b0b51fce619d64971437040bcd3964
IliyaKz <iliya-kuznetsov@mail.ru>
2022-08-04 22:44:05 +0300

    feat: add file with constants

---
+++ async-race/src/components/constants.ts
@@ -0,0 +1,7 @@
+export const carsNames: Array<string> = ['BMW', 'Ford', 'Honda', 'Lada', 'Mercedes', 'Opel', 'Kia', 'Nissan', 'Skoda', 'Suzuki', 'Fiat', 'Tesla', 'Volvo', 'Volkswagen', 'Lexus'];
+
+export const carsModels: Array<string> = ['X6', '7', 'Heavy', 'CD5', 'X101', 'HR', 'Prima', 'D2020', 'Buggy', 'Flash', 'CG5R', 'G77', 'Hunter', 'UY55', 'Road'];
+
+export const carsLimit = 7;
+
+export const winnersLimit = 10;
