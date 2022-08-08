0cb41b35d3a25b912f2b02e58517e3a3594f98ec
IliyaKz <iliya-kuznetsov@mail.ru>
2022-08-08 17:01:10 +0300

    fix: correct import in main ts file

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,10 +1,11 @@
-import './styles/style.css';
+import './styles/main.css';
 import { storage } from '../src/components/storage';
 import { ICarWithId, IWinnerWithCar } from './components/interfacesAndTypes';
-import { getPackOfCars, getPackOfWinners } from './components/api-component';
 import { listen } from './components/ui-components.ts/ui-listeners';
 import { render } from './components/ui-components.ts/ui-render';
 import { updateStateGarage } from './components/ui-components.ts/ui-functions';
+import { getPackOfCars } from './components/apy-components/cars-api';
+import { getPackOfWinners } from './components/apy-components/winners-api';
 
 function init() {
   document.addEventListener('DOMContentLoaded', () => {
